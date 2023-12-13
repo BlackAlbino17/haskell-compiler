8b6f45d1ab3502c38cb0f73f1e24cb0ecbbf90f9
BlackAlbino17 <blackalbino17@gmail.com>
2023-12-13 17:05:19 +0000

    compA and compB done

--- parser.hs
+++ parser.hs
@@ -1,16 +1,48 @@
 module Parser where 
+import Container 
+
 
 -- Part 2
 -- TODO: Define the types Aexp, Bexp, Stm and Program
 
--- compA :: Aexp -> Code
---compA = undefined -- TODO
+data Aexp = ADD Aexp Aexp
+          | SUB Aexp Aexp
+          | MULT Aexp Aexp
+          | Num Integer
+          deriving (Eq, Show)
 
--- compB :: Bexp -> Code
---compB = undefined -- TODO
+data Bexp = TRUE
+          | FALSE
+          | NOT Bexp
+          | AND Bexp Bexp
+          | LEQ Aexp Aexp
+          | Aexp
+          deriving (Eq, Show)
+
+compA :: Aexp -> Code
+compA (Parser.Num n)    = [Push n]
+compA (ADD e1 e2) = compA e1 ++ compA e2 ++ [Add]
+compA (SUB e1 e2) = compA e1 ++ compA e2 ++ [Sub]
+compA (MULT e1 e2) = compA e1 ++ compA e2 ++ [Mult]
+
+compB :: Bexp -> Code
+compB TRUE      = [Tru]
+compB FALSE     = [Fals]
+compB (NOT b)   = compB b ++ [Neg]
+compB (AND b1 b2) = compB b1 ++ compB b2 ++ [And]
+compB (LEQ e1 e2) = compA e1 ++ compA e2 ++ [Le]
+
 
 -- compile :: Program -> Code
---compile = undefined -- TODO
+compile = undefined -- TODO
+
 
 -- parse :: String -> Program
 --parse = undefined -- TODO
+
+test :: Aexp
+test = (MULT (ADD (Num 1) (Num 2)) (Num 3))    
+test1 = (SUB (MULT (ADD (Num 1) (Num 2)) (Num 3)) (Num 4))
+
+exampleBexp :: Bexp
+exampleBexp = AND (LEQ (Num 5) (Num 10)) (NOT TRUE)
