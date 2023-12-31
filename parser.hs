b32b24def815674066f321130b342cd02c14243d
João Sequeira <93724639+SequeiraJay@users.noreply.github.com>
2023-12-31 22:21:31 +0000

    error but implemented?

--- parser.hs
+++ parser.hs
@@ -1,5 +1,10 @@
 module Parser where 
 import Container 
+import Data.Char (isSpace, isDigit)
+import Data.List (elemIndex)
+import Text.Read (readMaybe)
+
+
 
 
 -- Part 2
@@ -19,6 +24,8 @@ data Bexp = TRUE
           | NOT Bexp
           | AND Bexp Bexp
           | LEQ Aexp Aexp
+          | Eq Aexp Aexp
+          | EqBool Bexp Bexp
           | Aexp
           deriving (Eq, Show)
 
@@ -42,6 +49,8 @@ data Stm = Assign String Aexp
          | Seq Stm Stm
          | IfThenElse Bexp Stm Stm
          | While Bexp Stm
+         | ParserWhile Bexp [Stm]
+         | ParserCond Bexp [Stm] [Stm]
          deriving (Eq, Show)
 
 
@@ -50,24 +59,210 @@ compile [] = []
 compile (stmt:rest) = case stmt of
   Assign var expr -> compA expr ++ [Store var] ++ compile rest
   Seq stmt1 stmt2 -> compile [stmt1] ++ compile [stmt2] ++ compile rest
-  IfThenElse cond thenStmt elseStmt ->
-    compB cond ++ [Branch (compile [thenStmt]) (compile [elseStmt])] ++ compile rest
-  While cond body ->
-    compile [body, While cond body] ++ [Branch (compile [body, While cond body]) [Noop]] ++ compile rest
+  IfThenElse evenBCond thenStmt elseStmt ->
+    compB evenBCond ++ [Branch (compile [thenStmt]) (compile [elseStmt])] ++ compile rest
+  While evenBCond body ->
+    compile [body, While evenBCond body] ++ [Branch (compile [body, While evenBCond body]) [Noop]] ++ compile rest
 
 
 
 -- parse :: String -> Program
 --parse = undefined -- TODO
 
+---------------------------------------------  LEXER e Get Aux ------------------------------------------------------------
+
+getValue :: Num a => Maybe a -> a
+getValue (Just a) = a+1
+getValue Nothing = error "Throw error getValue(Value not found)"
+
+lexer :: String -> [String]
+lexer = reverse . go []
+  where
+    go :: [String] -> String -> [String]
+    go acc [] = acc
+    go acc (c:cs)
+      | isSpace c = go acc cs
+      | isDigit c = let (token, rest) = span isDigit (c:cs)
+                    in go (token : acc) rest
+      | c `elem` "();+" = go ([c] : acc) cs
+      | c == '*'   = go ("*" : acc) cs
+      | otherwise = let (token, rest) = span (\x -> not (isSpace x || isDigit x || x `elem` "();+*")) (c:cs)
+                    in go (token : acc) rest
 
 
 
-testCompA :: Aexp
-testCompA = (SUB (MULT (ADD (Num 1) (Num 2)) (Num 3)) (Num 4))  
+parse :: String -> Program
+parse str = parsing(lexer str) []
 
-exampleBexp :: Bexp
-exampleBexp = AND (LEQ (Num (-5)) (Num 10)) (NOT TRUE)
+parsing :: [String] -> [Stm] -> [Stm]
+parsing [] stm = stm -- default
 
-exampleProgram :: [Stm]
-exampleProgram = [Assign "x" (Num 5), Assign "y" (ADD (Var "x") (Num 3))]
+
+
+--parsing dos assigns
+parsing (a:":=":rest) stm = let x = (getValue (elemIndex ";" (a:":=":rest)))
+                              in case parseSumOrProdOrIntOrPar (drop 2 (take (x-1) (a:":=":rest))) of
+                                Just (expr,[]) -> parsing (drop x (a:":=":rest)) (stm++[(Assign a (expr))])
+                                Nothing -> error "Parse Error"
+                                _ -> error "Parse Error"
+
+-- parsing ponto virgula
+parsing(";":rest) statement = parsing rest statement
+
+parsing ("(":rest) statement =
+  let closeParenIndex = getValue (elemIndex ")" ("(":rest))
+      arrayAfterCloseParen = drop closeParenIndex ("(":rest)
+      arrayInsideParens = drop 1 (take (closeParenIndex - 1) ("(":rest))
+  in parsing arrayAfterCloseParen (statement ++ parsing arrayInsideParens [])
+
+--parsing loops
+parsing("while":rest) statement = 
+  let ele = getValue(elemIndex "do" ("while":rest))
+      after = drop ele ("while":rest)
+      evenBCond = getValueBexp (parseAnd(isPar(drop 1(take(ele -1) ("while":rest)))))
+      parsingAux x = parsing(drop (getValue(elemIndex x after)) after)
+  in case firstElemTake after of
+      "(" -> parsingAux ")" (statement ++ [ParserWhile evenBCond (parsing (take (getValue (elemIndex ")" after)) after) [])])
+      _  -> parsingAux ";" (statement ++ [ParserWhile evenBCond (parsing (take (getValue (elemIndex ";" after)) after) [])])
+
+
+--parsing if statements
+parsing ("if":rest) statement =
+  let thenValue = getValue (elemIndex "then" ("if":rest))
+      elseValue = getValue (elemIndex "else" ("if":rest))
+      arrayAfter = drop elseValue ("if":rest)
+  in case firstElemTake arrayAfter of
+    "(" -> parsing (drop (getValue (elemIndex ")" arrayAfter)) arrayAfter) (statement ++ [ParserCond (getValueBexp (parseAnd (isPar (drop 1 (take (thenValue - 1) ("if":rest)))))) (parsing (drop thenValue (take (elseValue - 1) ("if":rest))) []) (parsing (take (getValue (elemIndex ")" arrayAfter)) arrayAfter) [])])
+    _   -> parsing (drop (getValue (elemIndex ";" arrayAfter)) arrayAfter) (statement ++ [ParserCond (getValueBexp (parseAnd (isPar (drop 1 (take (thenValue - 1) ("if":rest)))))) (parsing (drop thenValue (take (elseValue - 1) ("if":rest))) []) (parsing (take (getValue (elemIndex ";" arrayAfter)) arrayAfter) [])])
+
+firstElemTake :: [String] -> String
+firstElemTake [] = error"throw take first element"
+firstElemTake ("(":rest) = "("
+firstElemTake (a:rest) = a
+
+getValueBexp :: Maybe (Bexp, [String]) -> Bexp
+getValueBexp (Just (a, [")"])) = a
+getValueBexp (Just (a, [])) = a
+getValueBexp Nothing = error "throw getValueB error"
+
+isPar :: [String] -> [String]
+isPar [] = error"throw error is par"
+isPar ("(":rest) = drop 1 (take (length ("(":rest)) ("(":rest))
+isPar rest = rest
+
+------------------------------------------------------------ PARSING OPERAÇOES ARITMETICAS ------------------------------------------------------
+--SLIDES TEORICAS
+
+parseInt :: [String] -> Maybe(Aexp,[String])   -- parsing integer literals
+parseInt (n : rest) = case(readMaybe n :: Maybe Integer) of
+  Just f -> Just(Num f, rest)
+  Nothing -> Just (Var n, rest)
+parseInt _ = Nothing
+
+
+parseProdOrInt :: [String] -> Maybe(Aexp,[String]) -- parsing products
+parseProdOrInt token = case parseInt token of
+  Just(expr1,("*":restString1)) ->
+    case parseProdOrInt restString1 of
+      Just(expr2, restString2) ->
+        Just(MULT expr1 expr2, restString2)
+      Nothing -> Nothing
+  result -> result -- can be nothing or valid
+
+
+parseSumOrProdOrInt :: [String] -> Maybe(Aexp,[String]) -- parsing sums
+parseSumOrProdOrInt token = case parseProdOrInt token of
+  Just(expr1,"+":restString1) ->
+    case parseProdOrInt restString1 of
+      Just(expr2, restString2) ->
+        Just(ADD expr1 expr2, restString2)
+      Nothing                -> Nothing
+  result -> result -- can be nothing or valid
+
+parseIntOrParentExpr :: [String] -> Maybe(Aexp,[String]) -- parsing Parenthesised expressions
+parseIntOrParentExpr ("(": rest) = case parseSumOrProdOrIntOrPar rest of
+  Just(expr,(")":restString1)) -> Just (expr, restString1)
+  Just _ -> Nothing -- no closing paren
+  Nothing -> Nothing
+parseIntOrParentExpr (n:rest) =
+  case (readMaybe n :: Maybe Integer) of
+    Just f -> Just (Num f, rest)
+    Nothing -> Just (Var n, rest)
+parseIntOrParenExpr tokens = Nothing
+
+
+parseProdOrIntOrPar :: [String] -> Maybe (Aexp,[String])
+parseProdOrIntOrPar rest =
+  case parseIntOrParentExpr rest of
+    Just (expr1,("*":restString1)) ->
+      case parseProdOrIntOrPar restString1 of
+        Just (expr2,restString2) -> Just (MULT expr1 expr2, restString2)
+        Nothing -> Nothing
+    result -> result
+
+
+parseSumOrProdOrIntOrPar :: [String] -> Maybe (Aexp,[String])
+parseSumOrProdOrIntOrPar rest =
+  case parseProdOrIntOrPar rest of
+    Just (expr1,("+":restString1)) ->
+      case parseSumOrProdOrIntOrPar restString1 of
+        Just (expr2,restString2) -> Just (ADD expr1 expr2, restString2)
+        Nothing -> Nothing
+    Just (expr1,("-":restString1)) ->
+      case parseSumOrProdOrIntOrPar restString1 of
+        Just (expr2,restString2) -> Just (SUB expr1 expr2, restString2)
+        Nothing -> Nothing
+    result -> result
+
+
+
+  ------------------------------------------------------------ PARSING BOOLEANOS ------------------------------------------------------
+
+
+parseTrueOrFalse :: [String] -> Maybe(Bexp, [String])
+parseTrueOrFalse("True": rest) = Just(TRUE, rest)
+parseTrueOrFalse("False": rest) = Just(FALSE, rest)
+parseTrueOrFalse ("(":rest) =
+  case parseAnd rest of
+    Just (expr, ")":restString1) -> Just (expr, restString1)
+    _ -> Nothing
+parseTrueOrFalse rest =
+  case parseSumOrProdOrIntOrPar rest of
+    Just (expr1, op:restString1) ->
+      case parseSumOrProdOrIntOrPar restString1 of
+        Just (expr2, restString2) ->
+          case op of
+            "==" -> Just (Eq expr1 expr2, restString2)
+            "<=" -> Just (LEQ expr1 expr2, restString2)
+            _    -> Nothing
+        _ -> Nothing
+    _ -> Nothing
+
+
+parseNot :: [String] -> Maybe(Bexp, [String])
+parseNot("not":rest) = 
+  case parseTrueOrFalse rest of 
+    Just(expr1,restString1) ->
+      Just(NOT expr1, restString1)
+    results -> results
+parseNot rest = parseTrueOrFalse rest
+
+
+parseEqBool :: [String] -> Maybe(Bexp, [String])
+parseEqBool rest = case parseNot rest of 
+  Just(expr1, ("=":restString1)) ->
+    case parseEqBool restString1 of
+      Just(expr2, restString2) ->
+        Just(EqBool expr1 expr2, restString2)
+      Nothing -> Nothing
+  results -> results
+
+
+parseAnd :: [String] -> Maybe(Bexp, [String])
+parseAnd rest = case parseEqBool rest of
+  Just(expr1, ("and":restString1)) ->
+    case parseAnd restString1 of
+      Just(expr2, restString2) ->
+        Just(AND expr1 expr2, restString2)
+      Nothing -> Nothing
+  results -> results
--- tests.hs
+++ tests.hs
@@ -2,6 +2,7 @@ module Tests where
 
 import Container
 import Interpreter
+import Parser
 
 testAssembler :: Code -> (String, String)
 testAssembler code = (stack2Str stack, state2Str state)
@@ -21,17 +22,17 @@ runAllAssemblerTests = do
 
 
 -- To help you test your parser
---testParser :: String -> (String, String)
---testParser programCode = (stack2Str stack, state2Str state)
---  where (_,stack,state) = run(compile (parse programCode), createEmptyStack, createEmptyState)
+testParser :: String -> (String, String)
+testParser programCode = (stack2Str stack, state2Str state)
+  where (_,stack,state) = run(compile (parse programCode), createEmptyStack, createEmptyState)
 
 
---runAllParserTests :: IO ()
---runAllParserTests = do
---  print $ testParser "x := 5; x := x - 1;" == ("","x=4")
---  print $ testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1 else y := 2" == ("","y=2")
---  print $ testParser "x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;)" == ("","x=1")
---  print $ testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;" == ("","x=2")
---  print $ testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;" == ("","x=2,z=4")
---  print $ testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("","x=2,y=-10,z=6")
---  print $ testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1")
+runAllParserTests :: IO ()
+runAllParserTests = do
+  print $ testParser "x := 5; x := x - 1;" == ("","x=4")
+  print $ testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1 else y := 2" == ("","y=2")
+  print $ testParser "x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;)" == ("","x=1")
+  print $ testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;" == ("","x=2")
+  print $ testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;" == ("","x=2,z=4")
+  print $ testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("","x=2,y=-10,z=6")
+  print $ testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1")
